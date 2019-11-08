89d693fa6a3acebbeb665ad7aef6d755f841230c
momo <buss.hood@gmail.com>
2019-11-08 08:50:17 +0100

    List & dict

--- first.py
+++ first.py
@@ -6,3 +6,13 @@ print(sys.version)
 a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 print('Middle two:', a[3:5])
 print('All but ends:', a[1:7])
+
+# Ceci est byte
+b = b'h\x65llo'
+print(list(b))
+print(b)
+
+# Ceci est une chaine des caratères car il est semblable au langages humains
+c = 'a\u0300 propos'
+print(list(c))
+print(c)
